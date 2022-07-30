75feb86718f551b7e65b8bed5ecc4579cee0b87f
MikhailEgorov <egorovmail25gmail.com>
2022-07-30 15:21:24 +0300

    Initial Commit

---
+++ SpringAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimation
+//
+//  Created by Егоров Михаил on 30.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
